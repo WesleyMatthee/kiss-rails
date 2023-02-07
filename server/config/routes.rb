2a10e759c39c357726130cae7cb215fdf3adf66d
daejung90 <djung.ca@gmail.com>
2023-02-07 17:05:24 -0500

    rendering json data to be available in API

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'users/index'
+  get 'incident_types/index'
+  get 'incident/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index]
+  resources :incident_types, only: [:index]
+  resources :incident, only: [:index]
 end
